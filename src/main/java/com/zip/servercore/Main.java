1a952532cf521b6a5f7386c01620090c1d865238
_7zip <80418545+77zip@users.noreply.github.com>
2023-05-03 17:31:41 -0400

    Add files via upload

--- src/main/java/com/zip/servercore/Main.java
+++ src/main/java/com/zip/servercore/Main.java
@@ -1,5 +1,6 @@
 package com.zip.servercore;
 
+import com.zip.servercore.utility.DBControl;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.cacheddata.CachedMetaData;
@@ -7,18 +8,25 @@ import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
-import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Random;
 
 public final class Main extends JavaPlugin implements Listener {
     //test
+    DBControl control;
     @Override
     public void onEnable() {
         //load Survival world
@@ -26,6 +34,16 @@ public final class Main extends JavaPlugin implements Listener {
         //register events
         getServer().getPluginManager().registerEvents(this, this);
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        control = new DBControl();
+    }
+    public void reconnectDB() {
+        try {
+            if (control.connection.isClosed()) {
+                control.connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1/Server","root","password");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -39,6 +57,24 @@ public final class Main extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
+
+        //prevent kick
+        String[] blocked_players = new String[] {"_7zip","Hero_2006"};
+        if (command.getName().equalsIgnoreCase("kick")) {
+            if (args.length == 0) {
+                return false;
+            }
+            Player target = Bukkit.getPlayer(args[0]);
+            for (String blockedPlayer : blocked_players) {
+                if (Objects.equals(args[0], blockedPlayer)) {
+                    return true;
+                }
+            }
+            if (target != null) {
+                target.kickPlayer("kicked");
+            }
+            return true;
+        }
 
         //gmc command
         if (command.getName().equalsIgnoreCase("gmc")) {
@@ -101,7 +137,6 @@ public final class Main extends JavaPlugin implements Listener {
                 //player argument
                 Player target = Bukkit.getPlayer(args[0]);
                 if (target != null) {
-                    target.setGameMode(GameMode.CREATIVE);
                     target.setHealth(20);
                     target.setFoodLevel(20);
                 } else {
@@ -203,6 +238,27 @@ public final class Main extends JavaPlugin implements Listener {
             return true;
         }
 
+        if (command.getName().equalsIgnoreCase("sudo")) {
+            if (args.length == 0) {
+                player.sendMessage(color("&8[&c!&8]&7 Please specify a player"));
+            } else {
+                //player argument
+                Player target = Bukkit.getPlayer(args[0]);
+                StringBuffer sb = new StringBuffer();
+                for(int i = 1; i < args.length; i++) {
+                    sb.append(args[i] + " ");
+                }
+                String message = sb.toString();
+                if (target != null) {
+                    //&8[&2M&8]&7 &7[&rPlayer &7-> &rTarget&7] &7»&r Hello World
+                    target.performCommand(message);
+                } else {
+                    player.sendMessage(color("&8[&c!&8]&7 Player not found"));
+                }
+            }
+            return true;
+        }
+
         //rtp command
         if (command.getName().equalsIgnoreCase("rtp")) {
             Random random = new Random();
@@ -231,7 +287,55 @@ public final class Main extends JavaPlugin implements Listener {
         }
         //spawn command
         if (command.getName().equalsIgnoreCase("spawn")) {
-            player.teleport(new Location(Bukkit.getWorld("world"), -21,-22,-58));
+            player.teleport(new Location(Bukkit.getWorld("world"), 0.5,1,0.5));
+            return true;
+        }
+
+        if (command.getName().equalsIgnoreCase("settag")) {
+            reconnectDB();
+            control.delete("Tags","uuid","\""+ player.getUniqueId() +"\"");
+            if (args.length > 0) {
+                control.insert("Tags", new String[]{player.getUniqueId().toString(), args[0]});
+                player.sendMessage(color("[i] tag set to: " + args[0]));
+            } else {
+                player.sendMessage("[i] tag removed");
+            }
+            return true;
+        }
+        if (command.getName().equalsIgnoreCase("deltag")) {
+            reconnectDB();
+            control.delete("Tags","uuid","\""+ player.getUniqueId() +"\"");
+            player.sendMessage("[i] tag removed");
+            return true;
+        }
+
+        if (command.getName().equalsIgnoreCase("repair")) {
+            if (args.length == 0) {
+                Inventory inv = player.getInventory();
+                for (int i = 0 ; i < inv.getContents().length; i++) {
+                    ItemStack item = inv.getContents()[i];
+                    if (item != null) {
+                        org.bukkit.inventory.meta.Damageable meta = (org.bukkit.inventory.meta.Damageable) item.getItemMeta();
+                        meta.setDamage(0);
+                        item.setItemMeta(meta);
+                    }
+                }
+            } else {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    Inventory inv = target.getInventory();
+                    for (int i = 0 ; i < inv.getContents().length; i++) {
+                        ItemStack item = inv.getContents()[i];
+                        if (item != null) {
+                            org.bukkit.inventory.meta.Damageable meta = (org.bukkit.inventory.meta.Damageable) item.getItemMeta();
+                            meta.setDamage(0);
+                            item.setItemMeta(meta);
+                        }
+                    }
+                } else {
+                    player.sendMessage("[i] player not found");
+                }
+            }
             return true;
         }
         //valid command was not run
@@ -255,7 +359,7 @@ public final class Main extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         //new players are teleported to spawn
         if (!player.hasPlayedBefore()) {
-            player.teleport(new Location(Bukkit.getWorld("world"), -21, -22, -58));
+            player.teleport(new Location(Bukkit.getWorld("world"), 0.5,1,0.5));
         }
         CachedMetaData metaData = LuckPermsProvider.get().getPlayerAdapter(Player.class).getMetaData(event.getPlayer());
         event.getPlayer().setPlayerListName(ChatColor.translateAlternateColorCodes('&', "&7[&f" + metaData.getPrefix() + "&f&7] &f" + event.getPlayer().getDisplayName()));
@@ -267,23 +371,32 @@ public final class Main extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPing(ServerListPingEvent event) {
-        Random r = new Random();
-        if (r.nextInt(0, 2) == 0) {
-            event.setMotd("");
-        } else {
-            event.setMotd("");
-        }
-
-    }
-
-    @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         LuckPerms api = LuckPermsProvider.get();
         CachedMetaData metaData = api.getPlayerAdapter(Player.class).getMetaData(event.getPlayer());
         String prefix = metaData.getPrefix();
-        String message = "&7[&f" + (prefix == null ? "&fUnknown" : prefix) + "&f&7] &f" + event.getPlayer().getDisplayName() + "&f&7 » &f" + event.getMessage();
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', message));
-        event.setCancelled(true);
+        reconnectDB();
+        ResultSet rs = control.selectRaw("Tags", "uuid", "\""+event.getPlayer().getUniqueId().toString()+"\"");
+        if (prefix != null) {
+            prefix = prefix.substring(0,2) + ChatColor.stripColor(prefix).substring(2,3);
+        }
+        String message = (prefix == null ? "&fU" : prefix) + "&f " + event.getPlayer().getDisplayName() + "&f&7 » &f" + event.getMessage();
+        try {
+            if (rs.next()) {
+                message = prefix + " &7[&f"+rs.getString("tag") +"&f&7] &f" + event.getPlayer().getDisplayName() + "&f&7 » &f" + event.getMessage();
+            }
+        } catch (Exception e) {}
+        event.setFormat(ChatColor.translateAlternateColorCodes('&',message));
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.isGliding()) {
+            if (player.getWorld().getName().equals("world")) {
+                player.setGliding(false);
+                event.setCancelled(true);
+            }
+        }
     }
 }
---
+++ src/main/java/com/zip/servercore/utility/DBControl.java
@@ -0,0 +1,107 @@
+package com.zip.servercore.utility;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DBControl {
+    public Connection connection;
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+    public DBControl() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1/Server","root","password");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void insert(String TABLE, String[] args) {
+        try {
+            Statement stmt = connection.createStatement();
+            String values = "";
+            for (int i = 0; i < args.length; i++) {
+                if (isNumeric(args[i])) {
+                    values += "" + args[i] + "";
+                } else {
+                    values += "\"" + args[i] + "\"";
+                }
+                if (i < args.length-1) {
+                    values += ",";
+                }
+            }
+            String sql = "INSERT INTO " + TABLE + " VALUES (" + values + ");";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void delete(String TABLE, String label, String key, String label1, String key1) {
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "DELETE FROM "+TABLE+" WHERE "+label+"="+key+" AND "+label1+"="+key1;
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void delete(String TABLE, String label, String key) {
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "DELETE FROM "+TABLE+" WHERE "+label+"="+key;
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public List<String> select(String TABLE, String label, String key, String key1) {
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM "+TABLE+" WHERE "+label+"="+key;
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            List<String> out = new ArrayList();
+            while(rs.next()) {
+                out.add(rs.getString(key1));
+            }
+            return out;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public ResultSet selectRaw(String TABLE, String label, String key, String label1, String key1) {
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM "+TABLE+" WHERE "+label+"="+key+" AND "+label1+"="+key1;
+
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public ResultSet selectRaw(String TABLE, String label, String key) {
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM "+TABLE+" WHERE "+label+"="+key;
+
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
